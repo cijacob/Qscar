bd1e614fa2b9697d4c194512751f7dd9ca04f8dd
cijacob <jacobcindy@live.be>
2019-03-10 19:19:46 +0100

    Change to Postgresql DB and add Capistrano

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_03_08_215855) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "projects", force: :cascade do |t|
     t.string "name"
     t.text "description"
