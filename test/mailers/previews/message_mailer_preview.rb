cf287d6b2045db32bf449ac5c0dd4a2ee1072817
cijacob <jacobcindy@live.be>
2019-03-20 14:41:04 +0100

    Fix fooer and wip with email send

--- test/mailers/previews/message_mailer_preview.rb
+++ test/mailers/previews/message_mailer_preview.rb
@@ -3,11 +3,9 @@ class MessageMailerPreview < ActionMailer::Preview
 
   # Preview this email at http://localhost:3000/rails/mailers/message_mailer/contact_me
   def contact_me
-    message = Message.new name: '', 
-                          email: '',
-                          body: ''
+    message = Message.last
 
-    MessageMailer.contact_me message
+    MessageMailer.contact_me(message)
   end
 
 end
