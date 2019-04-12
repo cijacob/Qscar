981ecbfc44ea2e89b44fb5702767355be54082aa
cijacob <jacobcindy@live.be>
2019-04-12 12:38:21 +0200

    New test new psswd and gmail user

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,8 +99,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               =>"gmail.com",
-    :user_name            => ENV['EMAIL'],
-    :password             => ENV['EMAIL_PASSWORD'],
+    :user_name            => ENV['GMAIL'],
+    :password             => ENV['GMAIL_PASSWORD'],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
