c8c99a59a473ad4fb7b4f01e5451cfa5cb1644f1
cijacob <jacobcindy@live.be>
2019-04-14 07:51:32 +0200

    Add in env prod new line for test gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -102,8 +102,8 @@ Rails.application.configure do
     :address              => "smtp.gmail.com",
     :port                 => 587,
     :domain               =>"gmail.com",
-    :user_name            => ENV['GMAIL'],
-    :password             => ENV['GMAIL_PASSWORD'],
+    :user_name            => ENV["GMAIL"],
+    :password             => ENV["GMAIL_PASSWORD"],
     :authentication       => "plain",
     :enable_starttls_auto => true
   }
