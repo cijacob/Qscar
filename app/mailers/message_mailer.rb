15c298900076674157120a4743fe814d8aa47fc9
cijacob <jacobcindy@live.be>
2019-04-17 08:06:48 +0200

    change email address reception

--- app/mailers/message_mailer.rb
+++ app/mailers/message_mailer.rb
@@ -2,6 +2,6 @@ class MessageMailer < ApplicationMailer
 
   def reception_mail 
     @message = params[:message]
-    mail(from: @message.email, to: 'jacobcindy@live.be', subject:'Informations from contact form')
+    mail(from: @message.email, to: 'info@qscar.be', subject:'Informations from contact form')
   end 
 end
