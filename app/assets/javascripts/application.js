6da131631b507967ac2617aa4a504242d35ce3ac
cijacob <jacobcindy@live.be>
2020-03-09 15:28:10 +0100

    re-organise value svg and value section

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -98,4 +98,107 @@ document.addEventListener('DOMContentLoaded', (event) => {
 $(function(){ $(document).foundation(); });
 
 
+(function () {
+  $(document).ready(function () {
+    var qscarServices;
+    qscarServices = {
+      index: 0,
+      nextScreen: function () {
+        if (this.index < this.indexMax()) {
+          this.index++;
+          return this.updateScreen();
+        }
+      },
+      prevScreen: function () {
+        if (this.index > 0) {
+          this.index--;
+          return this.updateScreen();
+        }
+      },
+      updateScreen: function () {
+        this.reset();
+        this.goTo(this.index);
+        return this.setBtns();
+      },
+      setBtns: function () {
+        var  $nextBtn, $prevBtn;
+        $nextBtn = $('.next-screen');
+        $prevBtn = $('.prev-screen');
+        if (qscarServices.index === qscarServices.indexMax()) {
+          $nextBtn.prop('disabled', true);
+          $prevBtn.prop('disabled', false);
+        } else if (qscarServices.index === 0) {
+          $nextBtn.prop('disabled', false);
+          $prevBtn.prop('disabled', true);
+        } else {
+          $nextBtn.prop('disabled', false);
+          $prevBtn.prop('disabled', false);
+        }
+      },
+      goTo: function (index) {
+        $('.screen').eq(index).addClass('active');
+        return $('.dot').eq(index).addClass('active');
+      },
+      reset: function () {
+        return $('.screen, .dot').removeClass('active');
+      },
+      indexMax: function () {
+        return $('.screen').length - 1;
+      },
+      closeModal: function () {
+        $('.qscar-services, .shade').removeClass('reveal');
+        return setTimeout(() => {
+          $('.qscar-services, .shade').removeClass('show');
+          this.index = 0;
+          return this.updateScreen();
+        }, 200);
+      },
+      openModal: function () {
+        $('.qscar-services, .shade').addClass('show');
+        setTimeout(() => {
+          return $('.qscar-services, .shade').addClass('reveal');
+        }, 200);
+        return this.updateScreen();
+      } };
+
+    $('.next-screen').click(function () {
+      return qscarServices.nextScreen();
+    });
+    $('.prev-screen').click(function () {
+      return qscarServices.prevScreen();
+    });
+    $('.close').click(function () {
+      return qscarServices.closeModal();
+    });
+    $('.open-qscar-services').click(function () {
+      return qscarServices.openModal();
+    });
+    qscarServices.openModal();
+
+    return $(document).keydown(function (e) {
+      switch (e.which) {
+        case 37:
+          // left
+          qscarServices.prevScreen();
+          break;
+        case 38:
+          // up
+          qscarServices.openModal();
+          break;
+        case 39:
+          // right
+          qscarServices.nextScreen();
+          break;
+        case 40:
+          // down
+          qscarServices.closeModal();
+          break;
+        default:
+          return;}
+
+      e.preventDefault();
+    });
+  });
+
+}).call(this);
 
