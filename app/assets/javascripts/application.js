3f822b7a578ba2d6c84248f50e329b15937b2a87
cijacob <jacobcindy@live.be>
2020-03-19 23:53:22 +0100

    change structure complete

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -33,6 +33,53 @@ document.addEventListener('DOMContentLoaded', (event) => {
     } );
 
 });
+
+// links 
+let logo_link = document.querySelector('.navbar__logo')
+let our_project_link = document.querySelector(".our_project_link")
+let our_price_link = document.querySelector(".our_price_link")
+let about_us_link = document.querySelector(".about_us_link")
+let contact_us_link = document.querySelector(".contact_us_link")
+//section 
+let home = document.querySelector("#home")
+let our_project = document.querySelector("#our_project")
+let our_price = document.querySelector("#our_price")
+let about_us = document.querySelector("#about_us")
+let contact_us = document.querySelector("#contact_us")
+
+function handleOpacity(opacityZero, opacityOne, opacityTwo, opacityThree, opacityFour){
+  opacityZero.style.opacity= 1,
+  opacityOne.style.opacity = 0, 
+  opacityTwo.style.opacity = 0, 
+  opacityThree.style.opacity = 0,
+  opacityFour.style.opacity = 0 
+}
+
+function handleAnimation(){
+  for(let i=0; i<arguments.length; i++ ){
+    arguments[i].classList.remove('main-animation');
+    arguments[i].offsetWidth;
+    arguments[i].classList.add('main-animation');
+  }
+}
+
+logo_link.addEventListener('click', function(){
+  handleOpacity(home, our_project, our_price, about_us, contact_us)
+})
+our_project_link.addEventListener('click', function(){
+  handleOpacity(our_project, home, our_price, about_us, contact_us)
+})
+our_price_link.addEventListener('click', function(){
+  handleOpacity(our_price, home, our_project, about_us, contact_us)
+})
+about_us_link.addEventListener('click', function(){
+  handleOpacity( about_us, home, our_project, our_price, contact_us)
+})
+contact_us_link .addEventListener('click', function(){
+  handleOpacity( contact_us, home, our_project, our_price, about_us, contact_us)
+})
+
+
   //Form
 
   $('.input').on("focus blur", function() {
